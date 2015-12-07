9fd9d1386581d52bae17d6b8ff9896d112f71865
Santiago Torres <storres@setislcom>
2015-12-07 17:28:33 +0100

    First commit, all checkboxes on datatables

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).on('ready', function (e) {
+        /* Table select / checkboxes utility */
+    $('.checkboxs thead :checkbox').change(function(){
+        if ($(this).is(':checked'))
+        {
+            $('.checkboxs tbody :checkbox').prop('checked', true).parent().addClass('checked');
+            $('.checkboxs_actions').show();
+            jQuery( "input", $('.table').dataTable().fnGetNodes() ).prop('checked', true).parent().addClass('checked');
+        }
+        else
+        {
+            $('.checkboxs tbody :checkbox').prop('checked', false).parent().removeClass('checked');
+            $('.checkboxs tbody tr.selectable').removeClass('selected');
+            $('.checkboxs_actions').hide();
+            jQuery( "input", $('.table').DataTable().fnGetNodes() ).prop('checked', false).parent().removeClass('checked');
+  
+        }
+    });
+
+});
